02dc4102c48c0be00fc2fafb854d23590226438f
Bert Lee <bert825_work@163.com>
2016-03-08 21:00:25 +0800

    Add Test for Objects.

---
+++ src/test/java/sun/util/ObjectsTest.java
@@ -0,0 +1,100 @@
+package sun.util;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test for {@link Objects}.
+ *
+ * @author xingle
+ * @since 1.0
+ */
+public class ObjectsTest {
+
+    @Test(dataProvider = "equalsTestData")
+    public void equals(Object a, Object b, boolean expected) {
+        boolean result = Objects.equals(a, b);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DataProvider(name = "equalsTestData")
+    private static Object[][] equalsTestData() {
+        return new Object[][]{
+                {null, null, true}, // 相等
+                {null, "123", false},
+                {"qwe", null, false},
+
+                {"jklfd", "duire", false},
+                {"difkle", "difkle", true},
+                {"hello, " + "world", "hello, world", true},
+        };
+    }
+
+
+    @Test(dataProvider = "hashCodeTestData")
+    public void hashCode_(Object obj, int expected) {
+        int hashCode = Objects.hashCode(obj);
+        assertThat(hashCode).isEqualTo(expected);
+    }
+
+    @DataProvider(name = "hashCodeTestData")
+    private static Object[][] hashCodeTestData() {
+        return new Object[][]{
+                {null, 0},
+        };
+    }
+
+
+    /**
+     * 建议使用 {@link java.util.Arrays#hashCode(Object[])}
+     */
+    @Test(dataProvider = "hashTestData")
+    public void hash(int expected, Object[] values) {
+        int hashCode = Objects.hash(values);
+        assertThat(hashCode).isEqualTo(expected);
+    }
+
+    @DataProvider(name = "hashTestData")
+    private static Object[][] hashTestData() {
+        return new Object[][]{
+                {0, null},
+        };
+    }
+
+
+    @Test(dataProvider = "isNullTestData")
+    public void isNull(Object obj, boolean expected) {
+        boolean result = Objects.isNull(obj);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DataProvider(name = "isNullTestData")
+    private static Object[][] isNullTestData() {
+        return new Object[][]{
+                {null, true},
+                {new Object(), false},
+                {"", false},
+        };
+    }
+
+
+    @Test(dataProvider = "nonNullTestData")
+    public void nonNull(Object obj, boolean expected) {
+        boolean result = Objects.nonNull(obj);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DataProvider(name = "nonNullTestData")
+    private static Object[][] nonNullTestData() {
+        return new Object[][]{
+                {null, false},
+                {new Object(), true},
+                {"", true},
+        };
+    }
+
+}
