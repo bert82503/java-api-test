81925a4013696bc8d36cf2668625a473958eedd0
Bert Lee <bert825_work@163.com>
2016-09-20 10:51:06 +0800

    Adds unit test of Collections.unmodifiableXxx(...) for Unmodifiable Wrappers (不可变包装器)

--- src/test/java/com/google/common/base/JoinerTest.java
+++ src/test/java/com/google/common/base/JoinerTest.java
@@ -2,6 +2,7 @@ package com.google.common.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.Lists;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -30,21 +31,21 @@ public class JoinerTest {
     private static Object[][] joinTestData() {
         return new Object[][]{
                 {Collections.emptyList(), ""},
-                {Arrays.asList("initialCapacity=16"), "initialCapacity=16"},
+                {Collections.singletonList("initialCapacity=16"), "initialCapacity=16"},
                 {Arrays.asList(
                         "initialCapacity=16", // 初始容量
-                        "maximumSize=1024", // 设置容量最大大小，防止缓存被撑爆！
+                        "maximumSize=1024", // 容量最大大小，防止缓存被撑爆！
                         "concurrencyLevel=4", // 并发级别
                         "expireAfterWrite=0m", // 写入过期时间
                         "expireAfterAccess=0h", // 访问过期时间
-                        "refreshAfterWrite=0m", // 写入过期时间
-                        "recordStats" // 开启性能统计
-                ), "initialCapacity=16,maximumSize=1024,concurrencyLevel=4,expireAfterWrite=0m,expireAfterAccess=0h,refreshAfterWrite=0m,recordStats"},
+                        "refreshAfterWrite=1m", // 写入过期时间
+                        "recordStats" // 性能统计
+                ), "initialCapacity=16,maximumSize=1024,concurrencyLevel=4,expireAfterWrite=0m,expireAfterAccess=0h,refreshAfterWrite=1m,recordStats"},
         };
     }
 
     @Test(expectedExceptions = NullPointerException.class)
     public void join_Exception() {
-        Joiner.on(',').join((List<?>) null);
+        Joiner.on(',').join(Lists.newArrayList((String) null));
     }
 }
--- src/test/java/sun/util/CollectionsTest.java
+++ src/test/java/sun/util/CollectionsTest.java
@@ -1,12 +1,24 @@
 package sun.util;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.LinkedTransferQueue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +46,88 @@ public class CollectionsTest {
                 {Lists.newArrayList(7, 23, 10, 3), Arrays.asList(3, 7, 10, 23)},
         };
     }
+
+    // Unmodifiable Wrappers (不可变包装器)
+
+    @Test(expectedExceptions = UnsupportedOperationException.class,
+            dataProvider = "unmodifiableCollectionTestData")
+    public <E> void unmodifiableCollection(Collection<E> c, E newElement) {
+        assertThat(c).isNotNull();
+        assertThat(c).isEmpty();
+
+        // modify - 抛出 UnsupportedOperationException
+        c.add(newElement);
+    }
+
+    @DataProvider(name = "unmodifiableCollectionTestData")
+    private static Object[][] unmodifiableCollectionTestData() {
+        return new Object[][]{
+                // Collection
+                // Queue
+                {Collections.unmodifiableCollection(Queues.<String>newPriorityQueue()), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newConcurrentLinkedQueue()), StringUtils.EMPTY},
+                // Deque
+                {Collections.unmodifiableCollection(Lists.<String>newLinkedList()), StringUtils.EMPTY},
+                // BlockingQueue
+                {Collections.unmodifiableCollection(Queues.<String>newArrayBlockingQueue(1)), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newLinkedBlockingQueue(1)), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newLinkedBlockingDeque(1)), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newArrayDeque()), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newConcurrentLinkedQueue()), StringUtils.EMPTY},
+                {Collections.unmodifiableCollection(Queues.<String>newSynchronousQueue()), StringUtils.EMPTY},
+                // TransferQueue
+                {Collections.unmodifiableCollection(new LinkedTransferQueue<String>()), StringUtils.EMPTY},
+
+                // Set
+                {Collections.unmodifiableSet(Sets.<String>newHashSet()), StringUtils.EMPTY},
+                {Collections.unmodifiableSet(Sets.<String>newLinkedHashSet()), StringUtils.EMPTY},
+                {Collections.unmodifiableSet(
+                        Sets.newEnumSet(EnumSet.noneOf(MapEnum.class), MapEnum.class)), MapEnum.NONE},
+                // SortedSet
+                {Collections.unmodifiableSortedSet(Sets.<String>newTreeSet()), StringUtils.EMPTY},
+                // NavigableSet
+                {Collections.unmodifiableSet(Sets.<String>newConcurrentHashSet()), StringUtils.EMPTY}, // ConcurrentHashMap
+                {Collections.unmodifiableSet(new ConcurrentSkipListSet<String>()), StringUtils.EMPTY}, // ConcurrentSkipListMap
+
+                // List
+                {Collections.unmodifiableList(Lists.<String>newArrayList()), StringUtils.EMPTY},
+                {Collections.unmodifiableList(Lists.<String>newCopyOnWriteArrayList()), StringUtils.EMPTY},
+        };
+    }
+
+    @Test(expectedExceptions = UnsupportedOperationException.class,
+            dataProvider = "unmodifiableMapTestData")
+    public <E> void unmodifiableMap(Map<String, E> map, E newElement) {
+        assertThat(map).isNotNull();
+        assertThat(map).isEmpty();
+        // modify - 抛出 UnsupportedOperationException
+        map.put("", newElement);
+    }
+
+    @DataProvider(name = "unmodifiableMapTestData")
+    private static Object[][] unmodifiableMapTestData() {
+        return new Object[][]{
+                // Map
+                {Collections.unmodifiableMap(Maps.<String, String>newHashMap()), StringUtils.EMPTY},
+                {Collections.unmodifiableMap(Maps.<String, String>newLinkedHashMap()), StringUtils.EMPTY},
+                {Collections.unmodifiableMap(new WeakHashMap<String, String>(1)), StringUtils.EMPTY},
+                {Collections.unmodifiableMap(Maps.<String, String>newIdentityHashMap()), StringUtils.EMPTY},
+                {Collections.unmodifiableMap(Maps.newEnumMap(MapEnum.class)), MapEnum.NONE},
+                // ConcurrentMap
+                {Collections.unmodifiableMap(Maps.<String, String>newConcurrentMap()), StringUtils.EMPTY}, // ConcurrentHashMap
+                {Collections.unmodifiableMap(new ConcurrentHashMap<String, String>(1)), StringUtils.EMPTY},
+                // SortedMap
+                // NavigableMap
+                {Collections.unmodifiableNavigableMap(new ConcurrentSkipListMap<String, String>()), StringUtils.EMPTY},
+                {Collections.unmodifiableNavigableMap(Maps.<String, String>newTreeMap()), StringUtils.EMPTY},
+        };
+    }
+
+    private enum MapEnum {
+        NONE
+    }
+
+
+    // Synchronization Wrappers (同步包装器)
 
 }
