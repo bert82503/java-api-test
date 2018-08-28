cd89f0cface0df5b63f42c2e39b57554b297763d
Edward Lee <bert825_work@163.com>
2018-08-28 15:57:37 +0800

    [Test] Add null element to NPE of SortedSet

---
+++ src/test/java/sun/util/SetTest.java
@@ -0,0 +1,33 @@
+package sun.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Test of {@link java.util.Set}.
+ */
+public class SetTest {
+
+  @Test
+  public void sortedSet() {
+    SortedSet<String> sortedSet = new TreeSet<>();
+//    sortedSet.add(null);
+    sortedSet.add("");
+    sortedSet.add("set");
+
+    assertThat(sortedSet.size()).isEqualTo(2);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void sortedSetNPE() {
+    SortedSet<String> sortedSet = new TreeSet<>();
+    sortedSet.add(null);
+
+    assertThat(sortedSet.isEmpty()).isTrue();
+  }
+
+}
