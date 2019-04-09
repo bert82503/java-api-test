c5456b459465496167a9b9aaed17f3bb528dbbea
Edward Lee <bert825_work@163.com>
2019-04-09 19:50:52 +0800

    Add initialCapacity of HashSet, threshold of HashMap

--- src/test/java/sun/util/SetTest.java
+++ src/test/java/sun/util/SetTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -11,6 +13,18 @@ import java.util.TreeSet;
  * Test of {@link java.util.Set}.
  */
 public class SetTest {
+
+  @Test
+  public void initialCapacity() {
+    // HashMap threshold = 2
+    Set<String> set = new HashSet<>(2);
+    set.add("one");
+    assertThat(set.size()).isEqualTo(1);
+
+    // HashMap threshold = 4
+    set = new HashSet<>(4);
+    assertThat(set.size()).isEqualTo(0);
+  }
 
   @Test
   public void sortedSet() {
