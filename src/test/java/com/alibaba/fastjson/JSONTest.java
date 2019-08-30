12b4318ca4a1ddc56d5c7bdca2e5a829bb54819f
Edward Lee <bert825_work@163.com>
2019-08-30 15:16:19 +0800

    Add test of JSON.toJSON(obj)

--- src/test/java/com/alibaba/fastjson/JSONTest.java
+++ src/test/java/com/alibaba/fastjson/JSONTest.java
@@ -1,12 +1,13 @@
 package com.alibaba.fastjson;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.google.common.collect.Maps;
-
-import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Map;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.google.common.collect.Maps;
+import org.testng.annotations.Test;
 
 /**
  * Test of {@link JSON}.
@@ -16,26 +17,66 @@ import java.util.Map;
  */
 public class JSONTest {
 
-    @Test
-    public void toJSONString() {
-        BankParam bankParam = new BankParam().setEntryName("37236638385647");
-        Map<String, Object> map = Maps.newHashMapWithExpectedSize(1);
-        map.put("bankJob", bankParam);
-        String jsonString = JSON.toJSONString(map);
-        assertThat(jsonString).isEqualTo("{\"bankJob\":{\"entryName\":\"37236638385647\"}}");
+  static {
+    // 全局缺省的命名策略
+//        ParserConfig.getGlobalInstance().propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+  }
+
+  @Test
+  public void toJSONString() {
+    BankParam bankParam = new BankParam();
+    bankParam.setEntryName("37236638385647");
+    Map<String, Object> map = Maps.newHashMapWithExpectedSize(1);
+    map.put("bankJob", bankParam);
+    String jsonString = JSON.toJSONString(map);
+    assertThat(jsonString).isEqualTo("{\"bankJob\":{\"entryName\":\"37236638385647\"}}");
+
+    jsonString = JSON.toJSONString(bankParam);
+    assertThat(jsonString).isEqualTo("{\"entryName\":\"37236638385647\"}");
+  }
+
+  @Test
+  public void toJSON() {
+    BankParam bankParam = new BankParam();
+    bankParam.setEntryName("xxx");
+    // 公开的getter/setter方法
+    Object obj = JSON.toJSON(bankParam);
+    assertThat(obj).isInstanceOf(JSONObject.class);
+    JSONObject jsonObject = (JSONObject) obj;
+    assertThat(jsonObject.getString("entry_name")).isEqualTo("xxx");
+  }
+
+  @Test
+  public void parseObject() {
+    String text = "{\"entry_name\":\"37236638385647\"}";
+    BankParam bankParam = JSON.parseObject(text, BankParam.class, ParserConfig.getGlobalInstance());
+    assertThat(bankParam).isNotNull();
+    assertThat(bankParam.getEntryName()).isEqualTo("37236638385647");
+
+    text = "{\"entryName\":\"37236638385647\"}";
+    bankParam = JSON.parseObject(text, BankParam.class, ParserConfig.getGlobalInstance());
+    assertThat(bankParam).isNotNull();
+    assertThat(bankParam.getEntryName()).isEqualTo("37236638385647");
+  }
+
+//  @JSONType
+  private static class BankParam {
+    @JSONField(name = "entry_name")
+    private String entryName;
+
+    public String getEntryName() {
+      return entryName;
     }
 
-
-    private static class BankParam {
-        private String entryName;
-
-        public String getEntryName() {
-            return entryName;
-        }
-
-        public BankParam setEntryName(String entryName) {
-            this.entryName = entryName;
-            return this;
-        }
+    public void setEntryName(String entryName) {
+      this.entryName = entryName;
     }
+
+    @Override
+    public String toString() {
+      return "BankParam{" +
+          "entryName='" + entryName + '\'' +
+          '}';
+    }
+  }
 }
