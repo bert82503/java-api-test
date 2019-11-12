59dce84f761e99e33cdb84f73d7d0e826bd36255
Edward Lee <bert825_work@163.com>
2019-11-12 10:44:27 +0800

    [Mockito] 1. Let's verify some behaviour! 让我们验证一些行为！

--- src/test/java/org/mockito/VerifySomeBehaviour.java
+++ src/test/java/org/mockito/VerifySomeBehaviour.java
@@ -7,10 +7,15 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 /**
- * 1. Let's verify some behaviour!
+ * 1. Let's verify some behaviour! 让我们验证一些行为！
  * <p>
  * In reality, please don't mock the List class. Use a real instance instead.
  * 实际上，请不要模拟List类，而是使用真实的实例。
+ * <p>
+ * Once created, a mock will remember all interactions. Then you can selectively
+ * verify whatever interactions you are interested in.
+ * 一旦创建，模拟对象将记住所有交互。
+ * 然后，您可以有选择地验证您感兴趣的任何交互。
  *
  * @since 2019-11-11
  */
