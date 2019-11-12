7bc65099788a6e2d48190ec253f295dc6214b249
Edward Lee <bert825_work@163.com>
2019-11-12 10:34:33 +0800

    [Mockito] 1. Let's verify some behaviour!

--- src/test/java/org/mockito/VerifySomeBehaviour.java
+++ src/test/java/org/mockito/VerifySomeBehaviour.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 
 /**
  * 1. Let's verify some behaviour!
+ * <p>
+ * In reality, please don't mock the List class. Use a real instance instead.
+ * 实际上，请不要模拟List类，而是使用真实的实例。
  *
  * @since 2019-11-11
  */
