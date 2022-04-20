483f56422041151a869311879881ccff4829b7be
edwardlee03 <bert825_work@163.com>
2022-04-20 12:14:49 +0800

    improve FindFirstAndLastPositionOfElementInSortedArrayTest

--- src/test/java/leet/code/search/binary/FindFirstAndLastPositionOfElementInSortedArrayTest.java
+++ src/test/java/leet/code/search/binary/FindFirstAndLastPositionOfElementInSortedArrayTest.java
@@ -21,14 +21,17 @@ public class FindFirstAndLastPositionOfElementInSortedArrayTest {
     @DataProvider(name = "searchRangeTestData")
     private static Object[][] searchRangeTestData() {
         return new Object[][]{
+                // 发现目标
                 {new int[]{5,7,7,8,8,10}, 8, new int[]{3,4}},
                 {new int[]{7,7,8,8}, 8, new int[]{2,3}},
                 {new int[]{7,7,8,8}, 7, new int[]{0,1}},
                 {new int[]{5,7,7,8,9,10}, 8, new int[]{3,3}},
-                {new int[]{5,7,7,8,8,10}, 6, new int[]{-1,-1}},
-                {new int[]{}, 0, new int[]{-1,-1}},
-                // 未考虑到
+                // 边界，未考虑到
                 {new int[]{1}, 1, new int[]{0,0}},
+                // 未发现目标
+                {new int[]{5,7,7,8,8,10}, 6, new int[]{-1,-1}},
+                // 边界
+                {new int[]{}, 0, new int[]{-1,-1}},
         };
     }
 }
