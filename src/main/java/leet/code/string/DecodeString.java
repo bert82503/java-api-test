b93ff9563d06faf3bc7149e5e9997bc336170d7a
edwardlee03 <bert825_work@163.com>
2022-04-22 12:39:57 +0800

    improve DecodeString

--- src/main/java/leet/code/string/DecodeString.java
+++ src/main/java/leet/code/string/DecodeString.java
@@ -1,6 +1,7 @@
 package leet.code.string;
 
-import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 394. 字符串解码
@@ -21,6 +22,10 @@ import java.util.Stack;
  * * s 由小写英文字母、数字和方括号 '[]' 组成
  * * s 保证是一个 有效 的输入
  * * s 中所有整数的取值范围为 [1, 300]
+ * <p></p>
+ * 特性：
+ * 1. 本题难点在于括号内嵌套括号，需要从内向外生成与拼接字符串，这与栈的先入后出特性对应。
+ * 2. 编码的字符串中的关键词是连续的
  *
  * @author guangyi
  */
@@ -34,68 +39,56 @@ public class DecodeString {
      */
     private static final char LEFT_PARENTHESIS = '[';
 
+    /**
+     * 解法一：辅助栈法
+     *
+     * @param str 编码的字符串
+     * @return 解码后的字符串
+     */
     public static String decodeString(String str) {
         if (str == null || str.isEmpty()) {
             return str;
         }
+        int multi = 0;
+        StringBuilder result = new StringBuilder(16);
         // 堆栈
-        Stack<Character> stack = new Stack<>();
+        Deque<Integer> multiStack = new LinkedList<>();
+        Deque<String> resultStack = new LinkedList<>();
+        // 1.构建辅助栈 stack， 遍历字符串 s 中每个字符 c
         for (char ch : str.toCharArray()) {
-            // 1.右括号
-            if (ch == RIGHT_PARENTHESIS) {
-                // 出栈
-                ch = stack.pop();
-                // 2.大小写英文字母
-                StringBuilder letterStringBuilder = new StringBuilder(16);
-                while (Character.isLowerCase(ch) || Character.isUpperCase(ch)) {
-                    letterStringBuilder.append(ch);
-                    // 出栈
-                    ch = stack.pop();
-                }
-                // 逆向反转
-                letterStringBuilder = letterStringBuilder.reverse();
-                // 3.左括号
-                if (ch == LEFT_PARENTHESIS) {
-                    // 出栈
-                    ch = stack.pop();
-                }
-                // 4.数字
-                StringBuilder numStringBuilder = new StringBuilder(16);
-                while (Character.isDigit(ch)) {
-                    numStringBuilder.append(ch);
-                    if (stack.isEmpty()) {
-                        break;
-                    }
-                    // 出栈
-                    ch = stack.pop();
-                }
-                if (!stack.isEmpty()) {
-                    // 入栈
-                    stack.push(ch);
-                }
-                // 逆向反转
-                numStringBuilder = numStringBuilder.reverse();
-                int num = Integer.parseInt(numStringBuilder.toString());
-                // 5.解码
-                for (int i = 0; i < num; i++) {
-                    int length = letterStringBuilder.length();
-                    for (int j = 0; j < length; j++) {
-                        ch = letterStringBuilder.charAt(j);
-                        // 入栈
-                        stack.push(ch);
-                    }
-                }
-            } else {
+            if (ch >= '0' && ch <= '9') {
+                // 数字
+                // 当 c 为数字时，将数字字符转化为数字 multi，用于后续倍数计算
+                multi = multi * 10 + (ch - '0');
+            } else if (Character.isLowerCase(ch) || Character.isUpperCase(ch)) {
+                // 字母
+                // 当 c 为字母时，在 res 尾部添加 c
+                result.append(ch);
+            } else if (ch == LEFT_PARENTHESIS) {
+                // 左括号
                 // 入栈
-                stack.push(ch);
+                // 当 c 为 [ 时，将当前 multi 和 res 入栈，并分别置空置 0
+                //   记录此 [ 前的临时结果 res 至栈，用于发现对应 ] 后的拼接操作
+                //   记录此 [ 前的倍数 multi 至栈，用于发现对应 ] 后，获取 multi × [...] 字符串
+                //   进入到新 [ 后，res 和 multi 重新记录
+                multiStack.addLast(multi);
+                resultStack.addLast(result.toString());
+                multi = 0;
+                result = new StringBuilder(16);
+            } else if (ch == RIGHT_PARENTHESIS) {
+                // 右括号
+                // 出栈
+                // 当 c 为 ] 时，stack 出栈，拼接字符串 res = last_res + cur_multi * res
+                // last_res是上个 [ 到当前 [ 的字符串，例如 "3[a2[c]]" 中的 a
+                // cur_multi是当前 [ 到 ] 内字符串的重复倍数，例如 "3[a2[c]]" 中的 2
+                int curMulti = multiStack.removeLast();
+                StringBuilder tmp = new StringBuilder(curMulti * result.length() + 1);
+                for (int i = 0; i < curMulti; i++) {
+                    tmp.append(result);
+                }
+                result = new StringBuilder(resultStack.removeLast() + tmp);
             }
         }
-        // 出栈的结果
-        StringBuilder result = new StringBuilder(16);
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
-        }
-        // 逆向反转
-        return result.reverse().toString();
+        return result.toString();
     }
 }
