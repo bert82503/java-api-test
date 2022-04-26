7cf111ba6374a0beeb7ff1b4e88fce8fa230c604
edwardlee03 <bert825_work@163.com>
2022-04-26 15:39:56 +0800

    improve MaxProfit

--- src/main/java/leet/code/stack/monotonous/MaxProfit.java
+++ src/main/java/leet/code/stack/monotonous/MaxProfit.java
@@ -9,10 +9,10 @@ import java.util.LinkedList;
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
  * <p></p>
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
- *
+ * <p>
  * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
  * 设计一个算法来计算你所能获取的最大利润。
- *
+ * <p>
  * 返回你可以从这笔交易中获取的最大利润。
  * 如果你不能获取任何利润，返回 0 。
  * <p></p>
@@ -23,6 +23,38 @@ import java.util.LinkedList;
  */
 public class MaxProfit {
     /**
+     * 方法二：一次遍历
+     * <p></p>
+     * 我们来假设自己来购买股票。随着时间的推移，每天我们都可以选择出售股票与否。
+     * 那么，假设在第 i 天，如果我们要在今天卖股票，那么我们能赚多少钱呢？
+     * <p>
+     * 显然，如果我们真的在买卖股票，我们肯定会想：如果我是在历史最低点买的股票就好了！
+     * 太好了，在题目中，我们只要用一个变量记录一个历史最低价格 minPrice，我们就可以假设自己的股票是在那天买的。
+     * 那么我们在第 i 天卖出股票能得到的利润就是 prices[i] - minPrice。
+     * <p>
+     * 因此，我们只需要遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：
+     * 如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？
+     * 当考虑完所有天数之时，我们就得到了最好的答案。
+     *
+     * @param prices 价格整数数组
+     * @return 返回可以从这笔交易中获取的最大利润
+     */
+    public static int maxProfit(int[] prices) {
+        // 左侧过去历史的最低价
+        int minPrice = Integer.MAX_VALUE;
+        // 最大利润
+        int maxProfit = 0;
+        int profit;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+
+    /**
+     * 方法三：单调栈
+     * <p></p>
      * 利用哨兵👨‍✈️，维护一个单调栈📈(图解，直观掌握)
      * <p></p>
      * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/c-li-yong-shao-bing-wei-hu-yi-ge-dan-diao-zhan-tu-/
@@ -52,7 +84,7 @@ public class MaxProfit {
      * @param prices 价格整数数组
      * @return 返回可以从这笔交易中获取的最大利润
      */
-    public static int maxProfit(int[] prices) {
+    public static int maxProfit_MonotonicStack(int[] prices) {
         // 最大利润
         int maxProfit = 0;
         // 单调栈
