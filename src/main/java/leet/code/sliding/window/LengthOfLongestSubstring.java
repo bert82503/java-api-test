962e14ced2a4d186f48719098cc3f8aca8ceafc4
edwardlee03 <bert825_work@163.com>
2022-05-02 00:43:47 +0800

    improve LengthOfLongestSubstring

--- src/main/java/leet/code/sliding/window/LengthOfLongestSubstring.java
+++ src/main/java/leet/code/sliding/window/LengthOfLongestSubstring.java
@@ -47,6 +47,55 @@ import java.util.Arrays;
  */
 public class LengthOfLongestSubstring {
 
+    /**
+     * 无重复字符的最长子串
+     * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+     * <pre>
+     * 方法一：滑动窗口及优化
+     * 关键字：重复字符 --> 出现1次
+     * 模式识别1：一旦涉及出现次数，需要用到散列表
+     * 构造子串，散列表存下标
+     * 模式识别2：涉及子串，考虑滑动窗口
+     *
+     * 方法一：滑动窗口
+     * 思路和算法
+     * 我们先用一个例子考虑如何在较优的时间复杂度内通过本题。
+     * 我们不妨以示例一中的字符串 abcabcbb 为例，找出从每一个字符开始的，不包含重复字符的最长子串，那么其中最长的那个字符串即为答案。
+     * 发现了什么？如果我们依次递增地枚举子串的起始位置，那么子串的结束位置也是递增的！
+     *
+     * 这样一来，我们就可以使用「滑动窗口」来解决这个问题了：
+     * * 我们使用两个指针表示字符串中的某个子串（或窗口）的左右边界，
+     *   其中左指针代表着上文中「枚举子串的起始位置」，而右指针即为上文中的 rk；
+     * * 在每一步的操作中，我们会将左指针向右移动一格，表示 我们开始枚举下一个字符作为起始位置，
+     *   然后我们可以不断地向右移动右指针，但需要保证这两个指针对应的子串中没有重复的字符。
+     *   在移动结束后，这个子串就对应着 以左指针开始的，不包含重复字符的最长子串。
+     *   我们记录下这个子串的长度；
+     * * 在枚举结束后，我们找到的最长的子串的长度即为答案。
+     *
+     * 判断重复字符
+     * 在上面的流程中，我们还需要使用一种数据结构来判断 是否有重复的字符，常用的数据结构为哈希集合。
+     * 在左指针向右移动的时候，我们从哈希集合中移除一个字符，在右指针向右移动的时候，我们往哈希集合中添加一个字符。
+     * 至此，我们就完美解决了本题。
+     * </pre>
+     *
+     * 滑动窗口
+     * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-dong-chuang-kou-by-powcai/
+     * <pre>
+     * 这道题主要用到思路是：滑动窗口
+     *
+     * 什么是滑动窗口？
+     * 其实就是一个队列，比如例题中的 abcabcbb，进入这个队列（窗口）为 abc 满足题目要求，
+     * 当再进入 a，队列变成了 abca，这时候不满足要求。
+     * 所以，我们要移动这个队列！
+     *
+     * 如何移动？
+     * 我们只要把队列的左边的元素移出就行了，直到满足题目要求！
+     * 一直维持这样的队列，找出队列出现最长的长度时候，求出解！
+     *
+     * 下面介绍关于滑动窗口的万能模板，可以解决相关问题，相信一定可以对滑动窗口有一定了解！
+     * 模板虽好，还是少套为好！多思考！更重要！
+     * </pre>
+     */
     public static int lengthOfLongestSubstring(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
@@ -58,16 +107,16 @@ public class LengthOfLongestSubstring {
         Arrays.fill(charMap, -1);
 
         int length = str.length();
-        for (int j = 0, i = 0; j < length; j++) {
-            char ch = str.charAt(j);
+        for (int right = 0, left = 0; right < length; right++) {
+            char ch = str.charAt(right);
             if (charMap[ch] >= 0) {
                 // 出现重复字符，右滑N步
                 // 存在左滑回退N步的风险，故取最大值
-                i = Math.max(charMap[ch] + 1, i);
+                left = Math.max(charMap[ch] + 1, left);
             }
             // 找到一个可行解，右滑1步
-            charMap[ch] = j;
-            maxLength = Math.max(j - i + 1, maxLength);
+            charMap[ch] = right;
+            maxLength = Math.max(right - left + 1, maxLength);
         }
         return maxLength;
     }
@@ -80,28 +129,28 @@ public class LengthOfLongestSubstring {
         int maxLength = Integer.MIN_VALUE;
         // 字符计数器，用于不含有重复字符判断
         int[] charMap = new int[256];
-        int low = 0;
-        int high = 0;
-        while (high < str.length()) {
-            char ch = str.charAt(high);
+        int left = 0;
+        int right = 0;
+        while (right < str.length()) {
+            char ch = str.charAt(right);
             charMap[ch] += 1;
             if (charMap[ch] > 1) {
                 // 出现重复字符
                 // 发现一个子串
-                maxLength = Math.max(maxLength, high - low);
+                maxLength = Math.max(maxLength, right - left);
                 // 左指针右移
                 char lowChar;
                 do {
-                    lowChar = str.charAt(low++);
+                    lowChar = str.charAt(left++);
                     charMap[lowChar] -= 1;
                 } while (lowChar != ch);
             }
             // 右指针右移
-            high++;
+            right++;
         }
         // 字符全部不重复
-        if (high > low) {
-            maxLength = Math.max(maxLength, high - low);
+        if (right > left) {
+            maxLength = Math.max(maxLength, right - left);
         }
         return maxLength;
     }
