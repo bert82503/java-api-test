e945da4fb3f873bc239c400acf79d5a92a2bd302
edwardlee03 <bert825_work@163.com>
2022-05-01 18:01:36 +0800

    improve AddTwoNumbers

--- src/main/java/leet/code/list/AddTwoNumbers.java
+++ src/main/java/leet/code/list/AddTwoNumbers.java
@@ -11,6 +11,19 @@ package leet.code.list;
  * 请你将两个数相加，并以相同形式返回一个表示和的链表。
  * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  *
+ * 示例 1：
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ *
+ * 示例 2：
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ *
+ * 示例 3：
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ *
  * 提示：
  * 每个链表中的节点数在范围 [1, 100] 内
  * 0 <= Node.val <= 9
@@ -21,6 +34,8 @@ package leet.code.list;
  */
 public class AddTwoNumbers {
 
+    /**
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode root = new ListNode();
         ListNode node = root;
