fb47b9f98a962b37994515cf86c5ffbf980ae26b
edwardlee03 <bert825_work@163.com>
2022-05-04 16:30:19 +0800

    improve IsPalindrome

--- src/main/java/leet/code/math/IsPalindrome.java
+++ src/main/java/leet/code/math/IsPalindrome.java
@@ -39,9 +39,53 @@ package leet.code.math;
  */
 public class IsPalindrome {
 
+    /**
+     * 回文数
+     * https://leetcode-cn.com/problems/palindrome-number/solution/hui-wen-shu-by-leetcode-solution/
+     * <pre>
+     * 方法1：数字转换为字符串
+     * 正序字符串，反序字符串
+     *
+     * 方法1：数字转换为字符串 - 进阶
+     * 字符串对半对称比较
+     *
+     * 方法2：将数字本身反转
+     * 整数溢出问题
+     *
+     * 方法2：进阶 - 反转一半数字
+     * 奇数位，偶数位
+     *
+     * 方法一：反转一半数字
+     * 思路
+     * 映入脑海的第一个想法是将数字转换为字符串，并检查字符串是否为回文。
+     * 但是，这需要额外的非常量空间来创建问题描述中所不允许的字符串。
+     *
+     * 第二个想法是将数字本身反转，然后将反转后的数字与原始数字进行比较，
+     * 如果它们是相同的，那么这个数字就是回文。
+     * 但是，如果反转后的数字大于 \text{int.MAX}int.MAX，我们将遇到整数溢出问题。
+     *
+     * 按照第二个想法，为了避免数字反转可能导致的溢出问题，为什么不考虑只反转 int 数字的一半？
+     * 毕竟，如果该数字是回文，其后半部分反转后应该与原始数字的前半部分相同。
+     * </pre>
+     */
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        // 循环建立反转一半的数字
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        // 针对奇数位和偶数位的数字，分别判断是否为回文
+        // 奇数位：x == revertedNumber / 10
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
     private static final int MAX_RESULT = Integer.MAX_VALUE / 10;
 
-    public static boolean isPalindrome(int x) {
+    public static boolean isPalindrome_One(int x) {
         if (x < 0) {
             return false;
         }
