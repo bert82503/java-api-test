69e5d6ccbd90198e694cb1043a1d4e475024e592
edwardlee03 <bert825_work@163.com>
2022-05-05 01:01:48 +0800

    add RemoveElement, RemoveElementTest

---
+++ src/main/java/leet/code/pointer/RemoveElement.java
@@ -0,0 +1,72 @@
+package leet.code.pointer;
+
+/**
+ * 27. 移除元素
+ * <p></p>
+ * https://leetcode-cn.com/problems/remove-element/
+ * <pre>
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 说明：
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ *
+ * 示例 1：
+ * 输入：nums = [3,2,2,3], val = 3
+ * 输出：2, nums = [2,2]
+ * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。
+ *      例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+ *
+ * 示例 2：
+ * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+ * 输出：5, nums = [0,1,4,0,3]
+ * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ *      注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 提示：
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 50
+ * 0 <= val <= 100
+ * </pre>
+ *
+ * @author guangyi
+ */
+public class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+        // 双指针
+        int left = 0;
+        int right = length - 1;
+        while (left <= right) {
+            // 寻找最左侧等于 val 的元素
+            while (left <= right && nums[left] != val) {
+                left++;
+            }
+            // 寻找最右侧不等于 val 的元素
+            while (left <= right && nums[right] == val) {
+                right--;
+            }
+            if (left < right) {
+                // 互换元素
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        // 处理最后一个元素是否等于 val
+        if (left < length && nums[left] != val) {
+            return left + 1;
+        } else {
+            return left;
+        }
+    }
+}
